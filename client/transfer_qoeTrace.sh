0035dd17f2d82eeabdc77d85351f2bb318759e15
e-BitReadPaper <lwj20020422@163.com>
2025-02-27 12:10:42 +0800

    Delete client/transfer_qoeTrace.sh

--- client/transfer_qoeTrace.sh
+++
@@ -1,3 +0,0 @@
-scp -r tsinghua_shiwx17@219.243.208.60:~/wangchao/client/trace/ ./trace/219.243.208.60/ 
-scp -r tsinghua_shiwx17@194.29.178.14:~/wangchao/client/trace/ ./trace/194.29.178.14/
-scp -r tsinghua_shiwx17@200.19.159.34:~/wangchao/client/trace/ ./trace/200.19.159.34/ 
